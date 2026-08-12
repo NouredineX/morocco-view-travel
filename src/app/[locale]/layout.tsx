d5c661680e10c25876f289351090e0c6b82a421a
NouredineX <noureddine.x@gmail.com>
2026-08-12 15:22:33 +0100

    Optimize page load speed: convert all public images to compressed WebP format, delete original heavy files, update references, and defer third-party analytic script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -153,9 +153,9 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
         {/* Google Analytics Script */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-VNMF0EKF50"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
